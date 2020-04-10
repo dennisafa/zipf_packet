942e6115cb86053255f487abd4daf4abe88ba57c
Dennis Afanasev <dennisafa@nimbnode33.nimbus.seas.gwu.edu>
2020-04-10 18:19:15 +0000

    Updated

--- zipf_packets.py
+++ zipf_packets.py
@@ -1,21 +1,25 @@
 from scapy.all import *
+import numpy as np
+import sys
 
 num_flows = 100
 init_num_packets = 1000.0
 res = 0
 zipf_dist = []
 packets = []
-pkt_dump = PcapWriter("udp_zipf_test.pcap", append=True, sync=True)
+summation = 0
 
 for i in range (0, num_flows):
     res = init_num_packets * (1.0/(i+1))
+    summation += res
     zipf_dist.append(int(res))
 
-print(zipf_dist)
 for i in range(num_flows):
     ip_src = RandIP()._fix()
     ip_dest = RandIP()._fix()
     flow_packet = Ether()/IP(src = ip_src, dst = "10.0.0.0")/UDP(dport = 80, sport = 80)
     for j in range(zipf_dist[i]):
-        pkt_dump.write(flow_packet)
+        packets.append(flow_packet)
 
+np.random.shuffle(packets)
+wrpcap("udp_zipf_test.pcap", packets)
